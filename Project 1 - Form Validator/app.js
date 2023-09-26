84651d428c8c9336b25b45f4f57bc15ead5681cc
enriqTS <henrcraft@gmail.com>
2023-09-25 21:14:41 -0300

    add checking

---
+++ Project 1 - Form Validator/app.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(String(email.toLowerCase()));
+}
+
+function getFieldName(input) {
+    if(input.id === 'password2'){
+        return 'Password confirmation'
+    } else {
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
+}
+
+function checkRequired(inputArr) {
+    inputArr.forEach(input => {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+});
