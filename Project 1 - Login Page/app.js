68d95a88936c3b5cc0758829dba7a9ab96c3a2ac
enriqTS <henrcraft@gmail.com>
2023-09-26 21:28:11 -0300

    updated checking

--- Project 1 - Login Page/app.js
+++ Project 1 - Login Page/app.js
@@ -19,7 +19,11 @@ function showSuccess(input) {
 function isValidEmail(email) {
   const re =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email.toLowerCase()));
+    if (re.test(input.value.trim())) {
+      showSuccess(input);
+    } else {
+      showError(input, 'Email is not valid');
+    }
 }
 
 function getFieldName(input) {
@@ -31,13 +35,17 @@ function getFieldName(input) {
 }
 
 function checkRequired(inputArr) {
-    inputArr.forEach(input => {
-        if(input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
-        } else {
-            showSuccess(input);
-        }
-    });
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
 }
 
 form.addEventListener("submit", (e) => {
